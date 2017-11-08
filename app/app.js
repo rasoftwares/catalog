cab8581d6727a0fd5d9aae9e125574421ce56ffb
robinbennedict <robinbennedict@gmail.com>
2017-11-08 14:12:45 +0530

    Fixes for   - externalize css style for price strike through   - on selection of the product, display the product in product page   - test cases for the functions in app.js

--- app/app.js
+++ app/app.js
@@ -46,79 +46,93 @@ app.controller('appController', function($scope, $http, $filter, $routeParams, D
     $scope.enableSearch = false;
     $scope.currentPage = 0;
     $scope.itemsPerPage = 8;
-  //$scope.store = DataService.store;
+    //$scope.store = DataService.store;
     $scope.cart  = DataService.cart;
 
 
-    $scope.products = [];
+    $scope.products;
+    $scope.product;
 
+    var firebase = {
+        'url' : 'https://onetouch-d52d4.firebaseio.com/',
+        'environment' : 'dev',
+        'dataStore' : 'request',
+        'authKey' : 've8PdopndzS3yD35SMF6KAd4VKpHQuxUotXNeHGw',
+        'data' : '-KtQNH3Sf_ffvpoBLD-9/product'
+    };
 
+    $scope.appURL = firebase.url + firebase.environment + "/" + firebase.dataStore + "/" + firebase.data +".json?auth="+ firebase.authKey;
 
-    $scope.firebaseURL = 'https://onetouch-d52d4.firebaseio.com/';
-    $scope.environment = 'dev';
-    $scope.dataStore = 'request';
-    $scope.authKey = 've8PdopndzS3yD35SMF6KAd4VKpHQuxUotXNeHGw';
-    $scope.data = "-KtQNH3Sf_ffvpoBLD-9/product";
-    $scope.appURL = $scope.firebaseURL + $scope.environment + "/" + $scope.dataStore + "/" + $scope.data +".json?auth="+ $scope.authKey;
-    //var all_appURL = firebaseURL + environment + "/" + dataStore ;
-    //https://onetouch-d52d4.firebaseio.com/dev/request/-KkidwRuc2de6rQwz-mO/product.json?auth=ve8PdopndzS3yD35SMF6KAd4VKpHQuxUotXNeHGw
+    //This ID is used to pass the current product id between home to product screen.
+    $scope.currentProductID = 0;
 
-    $scope.getProducts = function(appURL){
+    $scope.setCurrentProductID = function(id) {
+        $scope.currentProductID = id;
+    };
 
-        $http.get($scope.appURL).then(function(response){
+    $scope.getProduct = function (id) {
+        if($scope.debug)
+            console.log("getProudct() ------>" + "currentProductID - " + $scope.currentProductID);
 
-                            $scope.products = response.data;
+        var arr = $scope.products[0];
 
+        for (var i = 0; i < arr.length; i++) {
+          if (arr[i].id == id) {
+                $scope.product = arr[i];
+          }
+        }
+    }
 
-                            $scope.getProduct = function (id) {
-                                  for (var i = 0; i < $scope.product.length; i++) {
-                                      if ($scope.product[i].id == id)
-                                          return $scope.product[i];
-                                  }
-                                return null;
-                              }
-                              if ($routeParams.id != null) {
-                                  $scope.product = $scope.getProduct($routeParams.id);
-                              }
-                                  //<---pagination-->
-                               for (var i = 0; i < $scope.products.length; i++) {
-                                     if (i % $scope.itemsPerPage === 0) {
-                                         $scope.products[Math.floor(i / $scope.itemsPerPage)] = [ $scope.products[i] ];
-                                     } else {
-                                         $scope.products[Math.floor(i / $scope.itemsPerPage)].push($scope.products[i]);
-                                     }
-                                 }
-                               $scope.range = function (start, end) {
-                                   var ret = [];
-                                   if (!end) {
-                                       end = start;
-                                       start = 0;
-                                   }
-                                   for (var i = start; i < end/$scope.itemsPerPage; i++) {
-                                       ret.push(i);
-                                   }
-                                   return ret;
-                               };
+    $scope.getProducts = function(appURL) {
+        if($scope.debug)
+            console.log("getProudcts() ------>" + "currentProductID - " + $scope.currentProductID);
 
-                               $scope.prevPage = function () {
-                                   if ($scope.currentPage > 0) {
-                                       $scope.currentPage--;
-                                   }
-                               };
+        $http.get($scope.appURL).then(function(response) {
 
-                             $scope.nextPage = function () {
-                                 if ($scope.currentPage < $scope.products.length/$scope.itemsPerPage- 1) {
-                                     $scope.currentPage++;
-                                 }
-                             };
+            $scope.products = response.data;
 
-                             $scope.setPage = function () {
-                                 $scope.currentPage = this.n;
-                             }
-                        }).catch(function(){
-                            console.log("Failed");
-                             $scope.status = 'Failed...';
-                        });
+              /*if ($routeParams.id != null) {
+                  $scope.product = $scope.getProduct($routeParams.id);
+              }*/
+                  //<---pagination-->
+               for (var i = 0; i < $scope.products.length; i++) {
+                     if (i % $scope.itemsPerPage === 0) {
+                         $scope.products[Math.floor(i / $scope.itemsPerPage)] = [ $scope.products[i] ];
+                     } else {
+                         $scope.products[Math.floor(i / $scope.itemsPerPage)].push($scope.products[i]);
+                     }
+                 }
+               $scope.range = function (start, end) {
+                   var ret = [];
+                   if (!end) {
+                       end = start;
+                       start = 0;
+                   }
+                   for (var i = start; i < end/$scope.itemsPerPage; i++) {
+                       ret.push(i);
+                   }
+                   return ret;
+               };
+
+               $scope.prevPage = function () {
+                   if ($scope.currentPage > 0) {
+                       $scope.currentPage--;
+                   }
+               };
+
+             $scope.nextPage = function () {
+                 if ($scope.currentPage < $scope.products.length/$scope.itemsPerPage- 1) {
+                     $scope.currentPage++;
+                 }
+             };
+
+             $scope.setPage = function () {
+                 $scope.currentPage = this.n;
+             }
+        }).catch(function(){
+            console.log("call to url " + $scope.appURL  + " Failed");
+             $scope.status = 'call to url  ' + $scope.appURL  + ' Failed';
+        });
     };
 
 });
--- spec/unit/appControllerSpec.js
+++ spec/unit/appControllerSpec.js
@@ -2,14 +2,15 @@ describe('Application Controller', function() {
 
 	var $scope;
 	var ctrl;
+	var firebase_URL = 'https://onetouch-d52d4.firebaseio.com/dev/request/-KtQNH3Sf_ffvpoBLD-9/product.json?auth=ve8PdopndzS3yD35SMF6KAd4VKpHQuxUotXNeHGw';
 
     beforeEach(module('catalogApp'));
 
     beforeEach(inject(function($controller, $injector){
 		$scope =  $injector.get('$rootScope');
 		ctrl = $controller('appController', {$scope: $scope});
-
 	}));
+
 
     describe('Configurations', function(){
             it('should define page title',function(){
@@ -27,5 +28,49 @@ describe('Application Controller', function() {
             it('should have search title', function(){
                 expect($scope.search_title).toBe('Go');
             });
+
+            it('should have appropriate firebase details', function() {
+                expect($scope.appURL).toBe(firebase_URL);
+            });
+
+            it('should validate a product with the discounted price', function() {
+                //TODO:
+
+            });
+            it('should validate a product without discount price', function() {
+                //TODO:
+            });
+            it('should validate setCurrentProductID()', function() {
+                $scope.setCurrentProductID(1);
+                expect($scope.currentProductID).toBe(1);
+            });
+
+            it('should get the right product from the array by getProduct()', function() {
+                 var response = [[
+                                     {"discount":"0","id":"1","image":"1","inventory":{},"itemcode":"0001","itemname":"Saree","name":"Saree","price":"1750","type":"Cotton Saree"},
+                                     {"discount":"15","id":"2","image":"2","inventory":{},"itemcode":"0002","itemname":"Banaras Saree","name":"Banaras Saree","price":"1750","type":"Cotton Saree"}
+                                     ]];
+                $scope.products = response;
+                $scope.setCurrentProductID(1);
+                $scope.getProduct(1);
+                expect($scope.product.id).toBe("1");
+                expect($scope.product.discount).toBe("0");
+
+                $scope.getProduct(2);
+                expect($scope.product.id).toBe("2");
+                expect($scope.product.discount).toBe("15");
+            });
+
+
+            /*
+            -- adhoc testing can be done here
+            it('Simple experiments', function() {
+                var arr1 = {"name":"name-1",
+                            "data":[{"data":"data-1"},{"data":"data-2"}]};
+                console.log(angular.isObject(arr1));
+                console.log(arr1.length + ":" + Array.isArray(arr1));
+                console.log(arr1.data.length + ":" + Array.isArray(arr1.data));
+
+            });*/
     });
 });
